5c15e28674e05d08eba1c85525aeeeae47f9ace6
spartajet <spartajet.guo@gmail.com>
2017-06-24 01:43:32 +0200

    javafx的插件实现完成

---
+++ gear-match-app/src/test/kotlin/com/spartajet/gear/match/test/ApplicationTest.kt
@@ -0,0 +1,12 @@
+package com.spartajet.gear.match.test
+
+import org.springframework.boot.test.context.SpringBootTest
+
+/**
+ * @description
+ * @create 2017-06-24 上午1:16
+ * @email spartajet.guo@gmail.com
+ */
+@SpringBootTest
+class ApplicationTest {
+}
